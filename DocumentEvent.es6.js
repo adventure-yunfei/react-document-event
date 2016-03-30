f07827ad92adac16fd3e096c46ce471ed1ff6e8d
yunfei <adventure030@gmail.com>
2016-03-30 10:09:29 +0800

    update event handler when component update

--- DocumentEvent.es6.js
+++ DocumentEvent.es6.js
@@ -1,35 +1,62 @@
-import Component from 'react-pure-render/component';
-import capitalize from 'lodash/capitalize';
+import {Component, PropTypes} from 'react';
 
-const supportedEvents = ['click', 'mouseup', 'mousedown', 'keyup', 'keydown'];
-const reactEventMap = {
-    ...(supportedEvents.reduce((result, evtName) => {
-        result[evtName] = `on${capitalize(evtName)}`;
-        return result;
-    }, {})),
-    mouseup: 'onMouseUp',
-    mousedown: 'onMouseDown',
-    keyup: 'onKeyUp',
-    keydown: 'onKeyDown'
-};
+const supportedEvents = ['onClick', 'onMouseUp', 'onMouseDown', 'onKeyUp', 'onKeyDown'];
+
+const _on_reg = /^on/;
+// map from react event name to actual browser event name
+// e.g. {"onMouseUp": "mouseup"}
+const reactEventMap = supportedEvents.reduce((result, reactEvtName) => {
+    result[reactEvtName] = reactEvtName.replace(_on_reg, '').toLowerCase();
+    return result;
+}, {});
 
 export default class DocumentEvent extends Component {
+    static propTypes = {
+        children: PropTypes.node
+    };
+
+    _listeningEvents = {};
+
     componentDidMount() {
         const props = this.props;
-        supportedEvents.map(evtName => {
-            if (props[reactEventMap[evtName]]) {
-                window.addEventListener(evtName, props[reactEventMap[evtName]]);
+        supportedEvents.map(reactEvtName => {
+            const evtHandler = props[reactEvtName];
+            if (evtHandler) {
+                const evtName = reactEventMap[reactEvtName];
+                window.addEventListener(evtName, evtHandler);
+                this._listeningEvents[reactEvtName] = evtHandler;
+            }
+        });
+    }
+
+    componentDidUpdate() {
+        const {props, _listeningEvents} = this;
+        supportedEvents.map(reactEvtName => {
+            const oldEvtHandler = _listeningEvents[reactEvtName],
+                newEvtHandler = props[reactEvtName];
+
+            if (oldEvtHandler !== newEvtHandler) {
+                const evtName = reactEventMap[reactEvtName];
+                if (oldEvtHandler) {
+                    window.removeEventListener(evtName, oldEvtHandler);
+                }
+                if (newEvtHandler) {
+                    window.addEventListener(evtName, newEvtHandler);
+                }
+
+                this._listeningEvents[reactEvtName] = newEvtHandler;
             }
         });
     }
 
     componentWillUnmount() {
-        const props = this.props;
-        supportedEvents.map(evtName => {
-            if (props[reactEventMap[evtName]]) {
-                window.removeEventListener(evtName, props[reactEventMap[evtName]]);
+        const {_listeningEvents} = this;
+        for (let reactEvtName in _listeningEvents) {
+            const evtHandler = _listeningEvents[reactEvtName];
+            if (evtHandler) {
+                window.removeEventListener(reactEventMap[reactEvtName], evtHandler);
             }
-        });
+        }
     }
 
     render() {
